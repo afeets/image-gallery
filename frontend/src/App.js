fcbc31d22da5078b3e1530775b2ec29cb8e05cab
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-06 17:20:21 +0700

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 
@@ -31,14 +33,24 @@ const App = () => {
       });
     
       setWord('');
-  }
+  };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter(( image ) => image.id !== id ));
+  };
+ 
   return (
     <div>
       <Header title="Images Gallery"/>
       <Search word = { word } setWord = { setWord } handleSubmit={ handleSearchSubmit }/>
-      {/* { !!images.length && <ImageCard  image={images[0]} />} */}
-      { images.map((image, i) => (<ImageCard key={i} image={image} /> ))}
+      {/* { !!images.length && <ImageCard  image={images[0]} />} */}      
+      <Container className='mt-4'>
+        <Row xs={1} md={2} lg={3}>
+          { images.map((image, i) => (
+            <Col className="pb-4" key={i}><ImageCard image={image} deleteImage={handleDeleteImage} /></Col> 
+          ))}
+        </Row>
+      </Container>      
     </div>
   );
 }
