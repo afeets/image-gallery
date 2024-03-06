aeb4f070c6ace13fe4c4c6ba65a1ca45827e8c3c
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-06 10:06:29 +0700

    Initial Search Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+  backgroundColor: 'lightblue'
+}
 
 const Header = ( props ) => {
   const { title } = props;
   return (
-    <Navbar bg="light" data-bs-theme="light">
-        <Navbar.Brand href="/">{ title }</Navbar.Brand>
-    </Navbar>
+    <Container>
+      <Navbar style={navbarStyle} data-bs-theme="light">
+          <Navbar.Brand href="/">{ title }</Navbar.Brand>
+      </Navbar>
+    </Container>
   )
 }
 
