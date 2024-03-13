57e998f1b5c0d743bfa81766a0ae6e7da8ed81e1
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-13 15:33:00 +0700

    Added Car Footer with Author and Link if available to Author Portfolio

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Nav } from "react-bootstrap";
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No Author Name';
+  const authorPortfolioUrl = image.user?.portfolio_url;
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={ image.urls.small } />
@@ -16,6 +19,10 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           <Button variant="secondary" onClick={() => saveImage(image.id)}>Save</Button>
         )}
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        { authorPortfolioUrl && <Nav.Link href={authorPortfolioUrl} target="_blank">{ authorName }</Nav.Link> }
+        { !authorPortfolioUrl && authorName }
+      </Card.Footer>
     </Card>
   );
 };
